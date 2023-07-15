bbd72467a369bf2bc45b20218137819e912b487e
Hitesh <hitesh@localhost.localdomain>
2023-07-15 21:30:04 +0530

    updated

--- app/Projects/page.tsx
+++ app/Projects/page.tsx
@@ -2,6 +2,8 @@ import { Project } from "@/types"
 import ProjectClient from "./ProjectClient"
 import getProjects from "@/actions/getProjects"
 
+export const revalidate = 0
+
 const Projects = async () => {
     const projects : Project[] | null = await getProjects()
     return (
