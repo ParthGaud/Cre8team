9dd5b309d8f4e70831e15a21ca99065a015c0088
Hitesh <hitesh@localhost.localdomain>
2023-07-19 16:53:55 +0530

    updated

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -11,7 +11,8 @@ interface NavItemsLabel{
 const NavItems: React.FC<NavItemsLabel> = ({
     label, onClick
 }) => {
-    const path = usePathname()
+    const path = usePathname().split("/")[1]
+    console.log(path)
     const router = useRouter()
     return (
         <div className={`flex items-center justify-center cursor-pointer px-3 hover:text-neutral-700 transition ${path === label && 'font-semibold'}`} onClick={onClick}>
